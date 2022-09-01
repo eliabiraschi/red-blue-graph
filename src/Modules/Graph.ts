4acc03c2970633cca2749e0c328d0ea9a0377aa4
Elia Biraschi <elia.biraschi@traxretail.com>
2022-09-01 10:57:45 +0200

    fix: indentation and naming
    - fixed indentation removing tabs
    - renamed variable from stack to queue to better reflect its use

--- src/Modules/Graph.ts
+++ src/Modules/Graph.ts
@@ -1,22 +1,22 @@
 enum COLORS {
-	RED,
-	BLUE
+  RED,
+  BLUE
 }
 
 export type Result = [boolean, boolean | null];
 export interface AdjacencyList {
-	[n: string]: Set<string>;
+  [n: string]: Set<string>;
 }
 
 interface ColorsMap {
-	[n: string]: COLORS;
+  [n: string]: COLORS;
 }
 
 const addNeighbor = (
-	acc: AdjacencyList,
-	path: string[],
-	node: string,
-	i: number,
+  acc: AdjacencyList,
+  path: string[],
+  node: string,
+  i: number,
 ): void => {
   if (i === path.length || i === -1) return;
   acc[node] = acc[node]
@@ -26,13 +26,13 @@ const addNeighbor = (
 
 export const generateAdjacencyList = (text: string): AdjacencyList => {
   const paths = text.split(/[\n,]/gi);
-  
+
   const adjacencyList = paths
     .reduce<AdjacencyList>((acc, pathString: string) => {
       if (!pathString.length) return acc;
 
       const path = pathString.split('-').map(n => n.trim());
-      
+
       if (path.length === 1) {
         const node = path[0];
         if (node.length === 0) throw new Error('Invalid node: cannot be an empty string.')
@@ -57,17 +57,17 @@ export const generateAdjacencyList = (text: string): AdjacencyList => {
 
 export const isBipartiteAndConnected = (adjacencyList: AdjacencyList): Result => {
   const colors: ColorsMap = {};
-  const stack: string[] = [Object.keys(adjacencyList)[0]];
-  while (stack.length > 0) {
-    const node = stack.shift();
-		if (!node) continue;
+  const queue: string[] = [Object.keys(adjacencyList)[0]];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (!node) continue;
     const color = colors[node] || COLORS.RED;
     const neighborColor = color === COLORS.BLUE ? COLORS.RED : COLORS.BLUE;
     for (let neighbor of adjacencyList[node].values()) {
       if (colors[neighbor] && colors[neighbor] !== neighborColor) return [false, null];
       if (colors[neighbor]) continue;
       colors[neighbor] = neighborColor;
-      stack.push(neighbor);
+      queue.push(neighbor);
     }
   }
 
